d3b9d437272d1610eb563c3c88f24142324d2fce
Ankushkr03 <152615824+Ankushkr03@users.noreply.github.com>
2026-04-24 17:53:03 +0530

    Change order service port configuration

--- Backend/Experiment-11/order-service/order_app.py
+++ Backend/Experiment-11/order-service/order_app.py
@@ -69,4 +69,6 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(port=5002, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
